8ad48151445577ebde5362871271e78f764d390b
AzureAD\NainarMohamed <nainarmy412@gmail.com>
2023-07-21 14:28:54 +0530

    chnages commit

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -28,7 +28,7 @@ const VideoDetail = () => {
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "1500px", position: "sticky", top: "86px" }}>
+          <Box sx={{ width: "1500px", position: "sticky", top: "96px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
