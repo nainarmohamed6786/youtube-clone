d17b67b25b72722d1b3d5a4ff55017a7e9420e34
AzureAD\NainarMohamed <nainarmy412@gmail.com>
2023-07-22 11:21:43 +0530

    width changes

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -28,7 +28,7 @@ const VideoDetail = () => {
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "70vw", position: "sticky", top: "96px" }}>
+          <Box sx={{ width: {sx:"100vw",md:"80vw"}, position: "sticky", top: "96px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
